3a427a5859fe6a1eca2f27e22531b4ca4a85a6c8
martastn <martast@student.agh.edu.pl>
2024-12-19 20:47:23 +0100

    map direction

--- oolab/src/main/java/agh/ics/oop/model/MapDirection.java
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -2,27 +2,33 @@ package agh.ics.oop.model;
 
 public enum MapDirection {
     NORTH,
+    NORTHEAST,
     EAST,
+    SOUTHEAST,
     SOUTH,
-    WEST;
+    SOUTHWEST,
+    WEST,
+    NORTHWEST;
 
-    static final Vector2d[] unitVectors = {     new Vector2d(0,1),
+    static final Vector2d[] unitVectors = {
+            new Vector2d(0,1),
+            new Vector2d(1,1),
+            new Vector2d(1,0),
+            new Vector2d(-1,-1),
             new Vector2d(0,-1),
+            new Vector2d(-1,-1),
             new Vector2d(-1,0),
-            new Vector2d(1,0)};
+            new Vector2d(-1,1),
+    };
 
-    public String getSymbol() {
-        return switch (this) {
-            case NORTH -> "^";
-            case SOUTH -> "v";
-            case WEST -> "<";
-            case EAST -> ">";
-        };
-    }
 
     public String toString() {
         return switch (this) {
             case NORTH -> "Północ";
+            case NORTHEAST -> "Północny-Wschód";
+            case NORTHWEST -> "Północny-Zachód";
+            case SOUTHEAST -> "Południowy-Wschód";
+            case SOUTHWEST -> "Południowy-Zachód";
             case SOUTH -> "Południe";
             case WEST -> "Zachód";
             case EAST -> "Wschód";
@@ -36,13 +42,35 @@ public enum MapDirection {
     public MapDirection previous(){
         return MapDirection.values()[(this.ordinal() + MapDirection.values().length - 1) % MapDirection.values().length];}
 
+    public MapDirection changeDirection(int n){
+
+        if (n==0) {return this;}
+
+        if (n <= 4) {
+            for (int i = 0; i < n; i++){
+                this.next();
+            }
+        }
+        else {
+            for (int i = 8; i > n; i--){
+                this.previous();
+            }
+        }
+
+        return this;
+    }
+
     public Vector2d toUnitVector(){
 
         return switch (this) {
             case NORTH -> unitVectors[0];
-            case SOUTH -> unitVectors[1];
-            case WEST -> unitVectors[2];
-            case EAST -> unitVectors[3];
+            case NORTHEAST -> unitVectors[1];
+            case EAST -> unitVectors[2];
+            case SOUTHEAST -> unitVectors[3];
+            case SOUTH -> unitVectors[4];
+            case SOUTHWEST -> unitVectors[5];
+            case WEST -> unitVectors[6];
+            case NORTHWEST -> unitVectors[7];
         };
     }
 }
